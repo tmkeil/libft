6e6cb88e983177e027e00523ce7f8119183f4fc9
Tobias Keil <tkeil@student.42heilbronn.de>
2025-04-02 13:10:50 +0200

    norminette fixes

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: tkeil <tkeil@student.42.fr>                +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2024/10/08 13:10:47 by tkeil             #+#    #+#             */
 /*   Updated: 2024/10/08 13:10:47 by tkeil            ###   ########.fr       */
 /*                                                                            */
@@ -17,14 +20,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	needle_len;
 
 	if (!haystack || !needle)
-    	return (NULL);
+		return (NULL);
 	if (!*needle)
-		return ((char *) haystack);
+		return ((char *)haystack);
 	needle_len = ft_strlen(needle);
 	while (*haystack && len-- >= needle_len)
 	{
 		if (ft_strncmp(haystack, needle, needle_len) == 0)
-			return ((char *) haystack);
+			return ((char *)haystack);
 		haystack++;
 	}
 	return (NULL);
